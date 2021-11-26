b8f4137a9e442270645faa1183ab04f2f7ba8f67
Daniel de Castro <daniel.castro09@hotmail.com>
2021-11-26 11:42:54 -0300

    Implementação de rastreamento de usuário; Criação de outras mensagens de erro.

--- lib/screens/transaction_form.dart
+++ lib/screens/transaction_form.dart
@@ -8,6 +8,7 @@ import 'package:alura_crashlytics/models/contact.dart';
 import 'package:alura_crashlytics/models/transaction.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
+import 'package:giffy_dialog/giffy_dialog.dart';
 import 'package:uuid/uuid.dart';
 
 class TransactionForm extends StatefulWidget {
@@ -137,25 +138,31 @@ class _TransactionFormState extends State<TransactionForm> {
     });
     final Transaction transaction =
         await _webClient.save(transactionCreated, password).catchError((e) {
-      FirebaseCrashlytics.instance.setCustomKey('exception', e.toString());
-      FirebaseCrashlytics.instance
-          .setCustomKey('http_body', transactionCreated.toString());
-      FirebaseCrashlytics.instance.recordError(e.message, null);
+      if (FirebaseCrashlytics.instance.isCrashlyticsCollectionEnabled) {
+        FirebaseCrashlytics.instance.setCustomKey('exception', e.toString());
+        FirebaseCrashlytics.instance
+            .setCustomKey('http_body', transactionCreated.toString());
+        FirebaseCrashlytics.instance.recordError(e.message, null);
+      }
 
       _showFailureMessage(context, message: e.message);
     }, test: (e) => e is HttpException).catchError((e) {
-      FirebaseCrashlytics.instance.setCustomKey('exception', e.toString());
-      FirebaseCrashlytics.instance
-          .setCustomKey('http_body', transactionCreated.toString());
-      FirebaseCrashlytics.instance.recordError(e.message, null);
+      if (FirebaseCrashlytics.instance.isCrashlyticsCollectionEnabled) {
+        FirebaseCrashlytics.instance.setCustomKey('exception', e.toString());
+        FirebaseCrashlytics.instance
+            .setCustomKey('http_body', transactionCreated.toString());
+        FirebaseCrashlytics.instance.recordError(e.message, null);
+      }
 
       _showFailureMessage(context,
           message: 'timeout submitting the transaction');
     }, test: (e) => e is TimeoutException).catchError((e) {
-      FirebaseCrashlytics.instance.setCustomKey('exception', e.toString());
-      FirebaseCrashlytics.instance
-          .setCustomKey('http_body', transactionCreated.toString());
-      FirebaseCrashlytics.instance.recordError(e.message, null);
+      if (FirebaseCrashlytics.instance.isCrashlyticsCollectionEnabled) {
+        FirebaseCrashlytics.instance.setCustomKey('exception', e.toString());
+        FirebaseCrashlytics.instance
+            .setCustomKey('http_body', transactionCreated.toString());
+        FirebaseCrashlytics.instance.recordError(e.message, null);
+      }
 
       _showFailureMessage(context);
     }).whenComplete(() {
@@ -171,9 +178,20 @@ class _TransactionFormState extends State<TransactionForm> {
     String message = 'Unknown error',
   }) {
     showDialog(
-        context: context,
-        builder: (contextDialog) {
-          return FailureDialog(message);
-        });
+      context: context,
+      builder: (contextDialog) => NetworkGiffyDialog(
+        image: Image.asset('images/error.gif', ),
+        title: Text(
+          'OPS',
+          style: TextStyle(fontSize: 22, fontWeight: FontWeight.w600),
+        ),
+        description: Text(
+          message,
+          textAlign: TextAlign.center,
+        ),
+        entryAnimation: EntryAnimation.TOP,
+        onOkButtonPressed: () => Navigator.pop(context),
+      ),
+    );
   }
 }
